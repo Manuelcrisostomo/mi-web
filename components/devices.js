d236b917b11768f32fcca96c313253b8093d4732
Manuel Crisostomo <manuelcrisostomovega@gmail.com>
2025-10-29 20:34:34 -0300

    Actualización: separación de formularios y nuevos componentes JS

--- components/devices.js
+++ components/devices.js
@@ -56,7 +56,7 @@ export function showUserDashboard() {
             <li class="nav-item"><a class="nav-link" id="navTipoMina">⛏️ Tipo de Mina</a></li>
             <li class="nav-item"><a class="nav-link" id="navGeoEmpresa">🌍 Geo / Empresa</a></li>
             <!-- 🔹 Panel administrador -->
-            <li class="nav-item"><a class="nav-link" id="navAdmin">🛠️ Panel Admin</a></li>
+            
             <!-- 🔹 NUEVOS BOTONES AÑADIDOS -->
             <li class="nav-item"><a class="nav-link" id="navUsuarios">👥 Usuarios</a></li>
             <li class="nav-item"><a class="nav-link" id="navGraficos">📊 Gráficos</a></li>
